eded2b863ed3abd32b48405bcc9bd5aca82f50dd
Felipe Tajima <felipebtajima05@gmail.com>
2025-03-13 20:25:45 -0300

    Tipos de dados
    Tipos de dados, ou seja se uma variável é string, number, objeto, boolean ou array.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,7 @@
-let nome = "Felipe";
-let idade = 20;
+let string = "Felipe Tajima";
+let number = 25.2;
+let boolean = true;
+let array = ['item1', 'item2', 'item3', 'item4', 'item5'];
+let objeto = {nome: 'Felipe', idade: 20};
 
-console.log(`${nome} ${idade}`)
+console.log(typeof array);
