9e254679a2adfa7466150742039c8d7674a7f950
Felipe Tajima <felipebtajima05@gmail.com>
2025-03-16 23:34:53 -0300

    Condicionais else if
    Apenas testando para reforçar o conhecimento em estrutura condicional com else if

--- javascript.js
+++ javascript.js
@@ -1,5 +1,11 @@
-let idade = 19;
+let idade = 60;
 
-if (idade >= 18 && idade < 60) {
+if (idade < 18 && idade > 0) {
+    console.log('Você é criança!');
+} else if (idade >= 18 && idade <= 60) {
     console.log('Você é adulto!');
+} else if (idade > 60) {
+    console.log('Você é um idoso!');
+} else {
+    console.log('Insira uma idade válida!')
 }
